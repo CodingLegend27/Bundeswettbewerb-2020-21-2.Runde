85caf11d4bcf4e207a96b317e7627a59a8b7d992
CodingLegend27 <waffler.christoph@gmail.com>
2021-04-04 19:22:21 +0200

    First try on a solution with maximum bipartit matching

--- Aufgabe 2/Bipartit_Matching.cpp
+++ Aufgabe 2/Bipartit_Matching.cpp
@@ -8,6 +8,8 @@ typedef vector<vpii> vvpii;
 
 #define what(x) cerr<<#x<<" is "<<x<<endl;
 
+const int INF = numeric_limits<int>::max()/2;
+
 // maximales bipartites, gewichtetes Matching
 // Verwendung der Ungarischen Methode
 
@@ -21,8 +23,8 @@ unordered_map<string, int> words2num;
 unordered_map<int, string> num2words;
 
 // Anzahl der verwendeten Obstnamen (wichtig für die Konvertierung der Namen)
-// Zähler beginnt bei MAXN+1, damit Schalen und Sorten nicht vertauscht werden können
-int counter_used_words = MAXN+1;
+// Zähler beginnt bei MAXN, damit Schalen und Sorten nicht vertauscht werden können
+int counter_used_words;
 
 void add_word_to_converter(string& word) {
 	if (words2num.find(word) == words2num.end()) {
@@ -30,7 +32,6 @@ void add_word_to_converter(string& word) {
 		
 		words2num[word] = counter_used_words;
 		num2words[counter_used_words] = word;
-
 		counter_used_words ++;
 	}
 }
@@ -62,6 +63,8 @@ int main() {
 	cin.tie(0);
 
 	cin >> MAXN;
+	// counter_used_words = MAXN;
+	counter_used_words = 0;
 
 	// Einlesen von Donalds Wünschen in einer Zeile
 	cin.ignore();
@@ -89,8 +92,16 @@ int main() {
 	// Adjazenzliste nicht so gut geeignet (wegen Laufzeit zum Ändern der Gewichtung eines Knoten)
 	
 	// Initialisieren eines 2D-Arrays mit 0
-	int adj [2*MAXN][2*MAXN];
-	memset(adj, 0, sizeof(adj));
+	// int adj [2*MAXN][2*MAXN];
+
+	// Adjazenzliste mit Obst --> Schale; d.h. adj[Obst][Schale]
+	int adj[MAXN][MAXN];
+	// memset(adj, -1000, sizeof(adj));
+	for (int i=0;i<MAXN;++i) {
+		for (int j=0;j<MAXN;++j) {
+			adj[i][j] = -INF;
+		}
+	}
 
 
 
@@ -116,15 +127,86 @@ int main() {
 
 		for (int sorte : obstsorten) {
 			for (int schale : obstschalen) {
+				schale--;
+				if (adj[sorte][schale] < 0) adj[sorte][schale] = 0;
 				adj[sorte][schale] ++;
-				adj[schale][sorte] ++;
+				
+				// adj[schale][sorte] ++;
 			}
 		}
 	}
 
+	for (int i=0;i<MAXN;++i) {
+		for (int j=0;j<MAXN;++j) {
+			if(adj[i][j]>0) cout<<"i: "<<num2words[i]<<" j: "<<j+1<<" -> "<<adj[i][j]<<"\n";
+		}
+	}	
 
 
-	
+
+	vi u(MAXN), v(MAXN), p(MAXN), way(MAXN);
+
+	// hungarian algorithm
+	for (int i = 0; i < MAXN; ++i) {
+		p[0] = i;
+		int j0 = 0;
+
+		vi minv (MAXN, INF);
+		vi used (MAXN, false);
+
+		do {
+			used[j0] = true;
+			int i0 = p[j0];
+			int delta = INF;
+			int j1;
+
+			for (int j = 0; j < MAXN; ++j) {
+				if (!used[j]) {
+					int cur = adj[i0][j] - u[i0] - v[j];
+
+					if (cur < minv[j]) {
+						minv[j] = cur;
+						way[j] = j0;
+					}
+
+					if (minv[j] < delta) {
+						delta = minv[j];
+						j1 = j;
+					}
+
+				}
+			}
+
+			for (int j = 0; j < MAXN; ++j) {
+				if (used[j]) {
+					u[p[j]] += delta;
+					
+					// what(v[j]); what(delta);
+					// avoid overflow error
+					if (abs(v[j] - delta) <= INF) v[j] -= delta;
+				}
+				else {
+					minv[j] -= delta;
+				}
+			}
+
+			j0 = j1;
+		
+		} while(p[j0] != 0);
+
+		do {
+			int j1 = way[j0];
+			p[j0] = p[j1];
+			j0 = j1;
+		} while (j0);
+	}
+
+	vpii result;
+	for (int i = 0; i < MAXN; ++i) {
+		result.push_back({p[i], i});
+
+		cout<<p[i]+1<<" -> "<<num2words[i]<<"\n";
+	}
 
 
 
